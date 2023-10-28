edaa379b6819c102e1e1dc18c4dde9f2c3cd5913
I admin <dirks.anton96@yandex.ru>
2023-10-28 20:31:52 +0300

    uncomment all file

--- lection9.py
+++ lection9.py
@@ -1,86 +1,88 @@
-# import json
+import json
 
-# with open("city.list.json", "r",encoding="utf-8") as file:
-#   data = json.load(file)
+with open("city.list.json", "r",encoding="utf-8") as file:
+  data = json.load(file)
 
 
 # ########################################
 # #1. Определить количество городов в файле
-# #print(len(data))
+print(len(data))
 
 # ########################################
 # #2. Создать словарь, где ключ — это код страны, а значение — количество городов.
 
-# def get_dict_country_with_count_cities(date: list) -> dict:
-#     list_of_country = []
-#     uniccountry = []
-#     dict_country = {}
-#     count = 0
+def get_dict_country_with_count_cities(date: list) -> dict:
+    list_of_country = []
+    uniccountry = []
+    dict_country = {}
+    count = 0
     
-#     for x in date:
-#         list_of_country.append(x["country"])
-#     uniccountry = set(list_of_country)
-#     uniccountry = list(sorted(uniccountry))
+    for x in date:
+        list_of_country.append(x["country"])
+    uniccountry = set(list_of_country)
+    uniccountry = list(sorted(uniccountry))
 
-#     date = list(sorted(date,key = lambda x: x["country"]))
+    date = list(sorted(date,key = lambda x: x["country"]))
 
-#     for i in uniccountry:
-#         count = 0
-#         for x in date:
-#             if i == x["country"]:
-#                 count += 1
-#                 dict_country[i] = count
-
-
-#     return dict_country
+    for i in uniccountry:
+        count = 0
+        for x in date:
+            if i == x["country"]:
+                count += 1
+                dict_country[i] = count
 
 
-# c = get_dict_country_with_count_cities(data)
+    return dict_country
 
 
-# def get_dict_country_with_count_cities_v2(date: list) -> dict:
-#     list_of_country = []
-#     uniccountry = []
-#     dict_country = {}
-#     count = 0
+c = get_dict_country_with_count_cities(data)
+
+print(c)
+
+
+def get_dict_country_with_count_cities_v2(date: list) -> dict:
+    list_of_country = []
+    uniccountry = []
+    dict_country = {}
+    count = 0
     
-#     # for x in date:
-#     #     list_of_country.append(x["country"])
-#     # uniccountry = set(list_of_country)
-#     # uniccountry = list(sorted(uniccountry))
+    # for x in date:
+    #     list_of_country.append(x["country"])
+    # uniccountry = set(list_of_country)
+    # uniccountry = list(sorted(uniccountry))
 
-#     for x in date:
-#         if x["country"] in dict_country:
-#           dict_country[x["country"]] += 1
-#         else: 
-#           dict_country[x["country"]] = 1
+    for x in date:
+        if x["country"] in dict_country:
+          dict_country[x["country"]] += 1
+        else: 
+          dict_country[x["country"]] = 1
            
-#         # if x["country"] in uniccountry:
-#         #    dict_country[x["country"]] = 0
+        # if x["country"] in uniccountry:
+        #    dict_country[x["country"]] = 0
 
-#     return dict_country
+    return dict_country
 
-# c = get_dict_country_with_count_cities_v2(data)
+c = get_dict_country_with_count_cities_v2(data)
 
 
-# print(c)
+print(c)
 
 # #######################################################################
 # # 3. Подсчитать количество городов в северном полушарии и в южном.
  
-# def get_enumenator_city(date:list):
-#     n_city = []
-#     s_city = []
-#     for x in date:
-#       if float(x["coord"]["lat"]) > 0 :
-#         n_city.append(x)
-#       else:
-#         s_city.append(x)
-#     c_n_city = len(n_city)
-#     c_s_city = len(s_city)
-#     stroka = "Количество городов в северном полушарии: " + str(c_n_city) +"  " + "Количество городов в южном полушарии: " + str(c_s_city)
-#     return stroka
+def get_enumenator_city(date:list):
+    n_city = []
+    s_city = []
+    for x in date:
+      if float(x["coord"]["lat"]) > 0 :
+        n_city.append(x)
+      else:
+        s_city.append(x)
+    c_n_city = len(n_city)
+    c_s_city = len(s_city)
+    stroka = "Количество городов в северном полушарии: " + str(c_n_city) +"  " + "Количество городов в южном полушарии: " + str(c_s_city)
+    return stroka
   
 
-# c = get_enumenator_city(data)
-# print(c)
+c = get_enumenator_city(data)
+print(c)
