15bd000a5dab1b29ef09af38d89e69f7dc4954e2
I admin <dirks.anton96@yandex.ru>
2023-11-12 18:00:54 +0300

    5 task

--- lesson12.py
+++ lesson12.py
@@ -51,10 +51,26 @@ class RealShop(AbstractShop):
     def all_products(self) -> Product:
         return self._value_in_shop
 
+class NonProductError(ValueError):
+    pass
+
+class Validator:
+
+    def __init__(self, product: Product):
+        self._product = product
+    
+    def is_valid(self):
+        try:
+            return isinstance(self._product, Product)
+        except ValueError:
+            raise NonProductError(f"переданый экземпляп не является экземпляром класса Product - NonProductError")
+        
 pizza_1 = Pizza(1,"Margarita",20.6,17,["майнез","кетчуп","сыр"])
 coffee_1 = Coffee(1,"якобс",14.5,0.3,"крепкий")
 
 pr = RealShop()
+
+validatator = Validator(pr)
 
 pr.add_product(pizza_1)
 pr.add_product(coffee_1)
@@ -63,7 +79,8 @@ print(pr.all_products())
 
 pr.sell_product(pizza_1)
 print(pr.all_products())
-    
+
+print(validatator.is_valid())
 
 
     
