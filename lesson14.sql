bff5e3ce93b9bcf05133acd0fa298fc1bc23e1cf
I admin <dirks.anton96@yandex.ru>
2023-11-20 12:45:06 +0300

    fix lesson14

--- lesson14.sql
+++ lesson14.sql
@@ -6,9 +6,9 @@ Use Test
 
 CREATE TABLE Users(
     id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(40) NOT NULL,
-    password VARCHAR(40) not NULL,
-    email VARCHAR(30),
+    username VARCHAR(40) NOT NULL UNIQUE,
+    password VARCHAR(40) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE,
     constraint email_check check (email regexp '^[0-9a-zA-Z-\._]+@[0-9a-zA-Z-\._]+'),
     constraint passwd_check check (length(password) >= 10)
 )
@@ -16,7 +16,7 @@ CREATE TABLE Users(
 CREATE TABLE Seller(
     id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     company VARCHAR(50) NOT NULL,
-    phone VARCHAR(30) NOT NULL,
+    phone VARCHAR(30) NOT NULL UNIQUE,
     constraint phone_check check (phone regexp '^[0-9]+$')
 ) 
 
