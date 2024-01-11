817f171f3b951333677521b67c2a98e9ddaf35f4
I admin <dirks.anton96@yandex.ru>
2024-01-11 20:52:55 +0300

    start23-24

---
+++ TMS_HW2324/posts/models.py
@@ -0,0 +1,58 @@
+import uuid
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+
+from django.db import models
+
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from ckeditor.fields import RichTextField
+
+
+class User(AbstractUser):
+    """
+    Наследуем все поля из `AbstractUser`
+    И добавляем новое поле `phone`
+    """
+    phone = models.CharField(max_length=11, null=True, blank=True)
+
+    class Meta:
+        db_table = "users"
+        
+def upload_to(instance: "Note", filename: str) -> str:
+    """Путь для файла относительно корня медиа хранилища."""
+    return f"{instance.uuid}/{filename}"
+
+
+class Note(models.Model):
+    # Стандартный ID для каждой таблицы можно не указывать, Django по умолчанию это добавит.
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    mode_time = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to=upload_to, null=True, blank=True, verbose_name="Превью")
+    # auto_now_add=True автоматически добавляет текущую дату и время.
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name="Владелец")
+    objects = models.Manager()  # Он подключается к базе.
+
+    class Meta:
+        # db_table = 'notes'  # Название таблицы в базе.
+        ordering = ['-created_at']  # Дефис это означает DESC сортировку (обратную).
+        indexes = [
+            models.Index(fields=("created_at",), name="created_at_index"),
+        ]
+
+@receiver(post_delete, sender=Note)
+def after_delete_note(sender, instance: Note, **kwargs):
+    if instance.image:
+        note_media_folder: pathlib.Path = (settings.MEDIA_ROOT / str(instance.uuid))
+
+        for file in note_media_folder.glob("*"):
+            file.unlink(missing_ok=True)
+        note_media_folder.unlink(missing_ok=True)
+    
+# Create your models here.
---
+++ TMS_HW2324/posts/views.py
@@ -0,0 +1,118 @@
+from django.shortcuts import render
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+
+from django.urls import reverse
+from django.http import HttpResponseRedirect, Http404,HttpRequest
+from django.core.handlers.wsgi import WSGIRequest
+
+from .models import Note, User
+from .service import create_note, filter_notes, queryset_optimization
+
+
+def home_page_view(request: WSGIRequest):
+    # Обязательно! каждая функция view должна принимать первым параметром request.
+    all_notes = Note.objects.all()  # Получение всех записей из таблицы этой модели.
+    
+    queryset = queryset_optimization(Note.objects.all())
+     
+    context: dict = {
+        "notes": all_notes
+    }
+    
+    #return render(request, "home.html", context)
+    return render(request, "home.html", {"notes": queryset[:100]})
+
+@login_required
+def create_note_view(request: WSGIRequest):
+    if request.method == "POST":
+        note = create_note(request)
+        # note = Note.objects.create(
+        #     title=request.POST["title"],
+        #     content=request.POST["content"],
+        # )
+        return HttpResponseRedirect(reverse('show-note', args=[note.uuid]))
+
+    # Вернется только, если метод не POST.
+    return render(request, "create_form.html")
+
+
+def show_note_view(request: WSGIRequest, note_uuid):
+    try:
+        note = Note.objects.get(uuid=note_uuid)  # Получение только ОДНОЙ записи.
+
+    except Note.DoesNotExist:
+        # Если не найдено такой записи.
+        raise Http404
+
+    return render(request, "note.html", {"note": note})
+# Create your views here.
+
+def delete_node(request: WSGIRequest, note_uuid):
+    try: 
+        note = Note.objects.get(uuid=note_uuid)
+        note.delete()
+        return HttpResponseRedirect("/")
+    except Note.DoesNotExist:
+        raise Http404
+
+def update_note_view(request: WSGIRequest, note_uuid):
+    try:
+        note = Note.objects.get(uuid=note_uuid)
+        
+        if request.method == "POST":
+            note.title = request.POST.get("title")
+            note.content = request.POST.get("content")
+            note.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "edit_form.html", {"note": note})
+    except Note.DoesNotExist:
+        raise Http404
+
+
+
+def show_about_view(request: WSGIRequest):
+    return render(request, "show-about.html")
+
+
+def register(request: WSGIRequest):
+    if request.method != "POST":
+        return render(request, "registration/register.html")
+    print(request.POST)
+    if not request.POST.get("username") or not request.POST.get("email") or not request.POST.get("password1"):
+        return render(
+            request,
+            "registration/register.html",
+            {"errors": "Укажите все поля!"}
+        )
+    print(User.objects.filter(
+            Q(username=request.POST["username"]) | Q(email=request.POST["email"])
+    ))
+    # Если уже есть такой пользователь с username или email.
+    if User.objects.filter(
+            Q(username=request.POST["username"]) | Q(email=request.POST["email"])
+    ).count() > 0:
+        return render(
+            request,
+            "registration/register.html",
+            {"errors": "Если уже есть такой пользователь с username или email"}
+        )
+
+    # Сравниваем два пароля!
+    if request.POST.get("password1") != request.POST.get("password2"):
+        return render(
+            request,
+            "registration/register.html",
+            {"errors": "Пароли не совпадают"}
+        )
+
+    # Создадим учетную запись пользователя.
+    # Пароль надо хранить в БД в шифрованном виде.
+    User.objects.create_user(
+        username=request.POST["username"],
+        email=request.POST["email"],
+        password=request.POST["password1"]
+    )
+    return HttpResponseRedirect(reverse('home'))
+
